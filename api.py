edbff76d77d6b91a2381158ff2548aec11e86bb6
stepa <stepanmouratoglou@gmail.com>
2026-04-19 21:54:40 +0300

    Update laliga features

--- api.py
+++ api.py
@@ -98,10 +98,9 @@ def MakePrediction(match:MatchFeatures):
         away=team_profiles_laliga[match.away_team]
         features=[
             home['Days_Rest'],
-            away['Days_Rest'],
             home['Avg_Scored_Last_5'],
             home['Avg_Shots_Last_5'],
-            home['Wins_Last_5'],
+            home['Avg_Shots_Conceded_Last_5'],
             away['Avg_Scored_Last_5'],
             away['Avg_Shots_Last_5'],
             away['Avg_Shots_Conceded_Last_5'],
@@ -128,7 +127,7 @@ def MakePrediction(match:MatchFeatures):
           home['Avg_Shots_Conceded_Last_5'],
           away['Avg_Scored_Last_5'],
           away['Avg_Conceded_Last_5'],
-          away['Avg_Shots__Last_5'],
+          away['Avg_Shots_Last_5'],
           away['Avg_Shots_Conceded_Last_5'],
           home['ELO Rating'],
           away['ELO Rating']

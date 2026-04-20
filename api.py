46051ec38f2b5a8512625485138d0da9ac37b3de
stepa <stepanmouratoglou@gmail.com>
2026-04-20 12:42:58 +0300

    Fix error

--- api.py
+++ api.py
@@ -145,6 +145,7 @@ def MakePrediction(match:MatchFeatures):
           home['Avg_Conceded_Last_5'],
           home['Avg_Shots_Last_5'],
           home['Avg_Shots_Conceded_Last_5'],
+          home['Wins_Last_5'],
           away['Avg_Scored_Last_5'],
           away['Avg_Conceded_Last_5'],
           away['Avg_Shots_Last_5'],
